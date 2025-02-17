2f43d247fec634cae2be1d0a48a09b0da5e05f46
Inès <charines@outlook.fr>
2025-02-17 21:46:27 +0100

    update all User config/control

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'is_teacher',
         'email',
         'password',
     ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="surname" class="col-md-4 col-form-label text-md-end">{{ __('Surname') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
+
+                                @error('surname')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
@@ -60,6 +74,29 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+
+                        <div class="row mb-3">
+                            <label class="col-md-4 col-form-label text-md-end">{{ __('Professeur ?') }}</label>
+
+                            <div class="col-md-6 d-flex align-items-center">
+                                <div class="form-check me-3">
+                                    <input class="form-check-input @error('is_teacher') is-invalid @enderror" type="radio" name="is_teacher" id="teacher_yes" value="1" {{ old('is_teacher') == '1' ? 'checked' : '' }} required>
+                                    <label class="form-check-label" for="teacher_yes">Oui</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input class="form-check-input @error('is_teacher') is-invalid @enderror" type="radio" name="is_teacher" id="teacher_no" value="0" {{ old('is_teacher') == '0' ? 'checked' : '' }} required>
+                                    <label class="form-check-label" for="teacher_no">Non</label>
+                                </div>
+
+                                @error('is_teacher')
+                                <span class="invalid-feedback d-block" role="alert">
+                                <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
