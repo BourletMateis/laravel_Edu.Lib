7b83c5a716c56c65a0c5b5273fae1f031e053e17
Inès <charines@outlook.fr>
2025-02-17 17:14:08 +0100

    add migration appointment

---
+++ database/migrations/2025_02_17_155849_create_appointment_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointment', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_student_id')->constrained('users');
+            $table->foreignId('user_teacher_id')->constrained('users');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->date('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointment');
+    }
+};
