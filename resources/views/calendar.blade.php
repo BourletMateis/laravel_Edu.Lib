2d1234c87641c06799bba99d998d976208be7591
Mateis B <mateisbourlet9@gmail.com>
2025-02-18 09:03:30 +0100

    add calendar css event

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -7,24 +7,30 @@
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
     <style>
         #calendar {
-            max-width: 900px;
-            margin: 40px auto;
+            max-width: 1000px;
+            margin: 50px auto;
+        }
+        .fc-daygrid-event {
+            background-color: #378006;
+            color: #fff;
         }
     </style>
 </head>
 <body>
     <h2 style="text-align: center;">Calendrier des Réservations</h2>
     <div id='calendar'></div>
-
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             var calendarEl = document.getElementById('calendar');
             var calendar = new FullCalendar.Calendar(calendarEl, {
                 initialView: 'dayGridMonth', 
+                displayEventStart: true,
+                displayEventEnd: true,
+                eventOrder: 'start',
+                locale: 'fr',
                 events: '/booking',  
                 eventColor: '#378006', 
                 eventClick: function(info) {
-                    alert("Début : " + info.event.start + "\nFin : " + info.event.end);
                 }
             });
             calendar.render();
