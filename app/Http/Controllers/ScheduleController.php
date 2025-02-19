c5b838073b0ac94469c6c4bd3ab5322fe9dddabf
Mateis B <mateisbourlet9@gmail.com>
2025-02-19 21:48:15 +0100

    add store

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -25,10 +25,33 @@ class ScheduleController extends Controller
 
     }
 
-
     public function store(ScheduleRequest $request) // Utilisation de ScheduleRequest
     {
+        // Vérifier si l'horaire existe déjà pour cet enseignant
+        $exists = Schedule::where('user_teacher_id', Auth::id())
+            ->where('day', $request->day)
+            ->where('time_start', $request->time_start)
+            ->where('time_end', $request->time_end)
+            ->exists();
+
+        if ($exists) {
+            return redirect()->route('schedules.index')
+                ->with('error', 'Cet horaire existe déjà.');
+        }
+
+        // Enregistrement de l'horaire dans la base de données
+        Schedule::create([
+            'user_teacher_id' => Auth::id(),
+            'booked' => false,
+            'day' => $request->day,
+            'time_start' => $request->time_start,
+            'time_end' => $request->time_end,
+        ]);
+
+        // Redirection avec un message de succès
+        return redirect()->route('schedules.index')->with('success', 'Horaire ajouté');
     }
+
 
 
     public function destroy(Schedule $schedule) {
