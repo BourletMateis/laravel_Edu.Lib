6d371b232af56b518552571f4d4a72e7f861c688
NaKyoo <axel.letourneur-chiffoleau@edu.esiee-it.fr>
2025-02-20 16:08:09 +0100

    fix policies on reservation page

--- app/Http/Controllers/ProfesseurController.php
+++ app/Http/Controllers/ProfesseurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointments;
 use Illuminate\Http\Request;
 use App\Models\User;  // Utilisation du modèle User au lieu de Professeur
 
@@ -9,7 +10,7 @@ class ProfesseurController extends Controller
 {
     public function index()
     {
-        $this->authorize('view', User::class); // Autoriser l'accès en utilisant la politique UserPolicy
+        $this->authorize('view', new Appointments()); // Autoriser l'accès en utilisant la politique UserPolicy
 
         // Récupérer uniquement les utilisateurs où 'is_teacher' est égal à 1
         $professeurs = User::where('role', "teacher")->get();
